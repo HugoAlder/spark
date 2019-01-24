2236fe7f86bd96fb87095cf822ab4e6bf3d69978
Hugo Alder <hugoalder0@gmail.com>
2019-01-24 16:54:39 +0100

    Commented optional code

--- my-spark.py
+++ my-spark.py
@@ -1,5 +1,6 @@
 from pyspark import SparkContext
 from operator import add
+
 import shutil
 
 # In case of empty value
@@ -22,10 +23,12 @@ rdd = tf.map(lambda line: line.split(";")).map(lambda line: (line[3], to_int(lin
 # rdd = rdd.reduceByKey(lambda a, b: a + b)
 rdd = sc.parallelize(sorted(rdd.reduceByKey(lambda a, b: a + b).collect()))
 
+# To delete output directory in the HDFS : hdfs dfs -rm -r output
+
 # Saving
-try:
-    shutil.rmtree("output")
-except FileNotFoundError:
-   print("Output directory does not exist")
+# try:
+#     shutil.rmtree("output")
+# except FileNotFoundError:
+#   print("Output directory does not exist")
 
 rdd.saveAsTextFile("output")
